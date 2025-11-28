a5b594d4cd1e4692da1013cc5e4d7ceed3840073
Nishant2116 <nishantd369@gmail.com>
2025-11-29 02:10:06 +0530

    Allow all hosts and fix SSL proxy headers

--- backend/codeamon_audio_app/settings.py
+++ backend/codeamon_audio_app/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-3#_ew0s1lb1q#^!f5_9=-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',') + ['*.railway.app']
+# Fix for Railway/Proxy SSL
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
