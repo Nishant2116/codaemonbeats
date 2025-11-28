630e6127c88a3ecaabec0595f197e7927b92ffc1
Nishant2116 <nishantd369@gmail.com>
2025-11-29 01:05:52 +0530

    Add Railway deployment configuration files

---
+++ backend/start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Install dependencies
+pip install -r requirements.txt
+
+# Run migrations
+python manage.py migrate --noinput
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Start gunicorn
+gunicorn codeamon_audio_app.wsgi --log-file -
