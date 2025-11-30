4f6c9b3d2a465f1040fa5e15b67ded34e3e038a9
Nishant2116 <nishantd369@gmail.com>
2025-12-01 01:00:08 +0530

    Add AllowAny as default permission class

--- backend/codeamon_audio_app/settings.py
+++ backend/codeamon_audio_app/settings.py
@@ -164,6 +164,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
 }
 
 # Default primary key field type
